91511ca85658ef2760d718dbeef6ab67bc797073
Subzero0805 <68710182+Subzero0805@users.noreply.github.com>
2020-08-14 22:23:39 +0100

    Update index.js
    Written code in Learn IDE

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // write your code below!
+/* function returns "Happy Holidays" */
+function happyHolidays() {
+  return "Happy holidays!"
+}
+/* function returns "Happy holidays + name!" */
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+/* returns "Happy customholiday, + name!" */
+function happyCustomHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+/* return the number days to a specified holiday */
+function holidayCountdown (holiday, days){
+  return `It's ${days} days until ${holiday}!`
+}
